207c1f1e68240c07bee7dfb081f08dbda4329746
deejayy <deejayy@deejayy.hu>
2025-07-30 22:11:37 +0200

    Add ANSI color and RGB support

--- main.js
+++ main.js
@@ -6,37 +6,181 @@ export default class Configurator {
   currentTheme = null;
   paletteCatalog = [];
   themeCatalog = [];
+  tooltipTimer = null;
+  swatch = [];
 
   init = async () => {
     this.paletteCatalog = await this.loadCatalog(this.PALETTE_FOLDER).then(this.listPalettes);
     this.themeCatalog = await this.loadCatalog(this.THEME_FOLDER).then(this.listThemes);
     this.loadPaletteFromURL('./monokai.xml');
     this.loadThemeFromURL('./default.farconfig');
+    this.generateAnsi256Palette();
 
     this.bindHover();
     this.bindClick();
     this.bindColorChange();
+    this.bindColorHover();
+  };
+
+  getColorUsers = (colorClass, layer) => {
+    const color = colorClass;
+    const users = this.currentTheme.filter((i) => i[layer] === color).map((i) => i.name);
+    return users.length > 0 ? users.join('<br>') : undefined;
+  };
+
+  bindColorHover = () => {
+    document.querySelectorAll('.boxes').forEach((el) => {
+      el.addEventListener('mouseover', (event) => {
+        clearTimeout(this.tooltipTimer);
+        if (
+          event.target.classList.contains('cbox') ||
+          event.target.classList.contains('acbox') ||
+          event.target.classList.contains('swatch')
+        ) {
+          this.tooltipTimer = setTimeout(() => {
+            const tooltip = document.getElementById('tooltip');
+            const layer = event.target.closest('.boxes').classList.contains('fg') ? 'fg' : 'bg';
+            const users = event.target.closest('.swatches')
+              ? this.getColorUsers(event.target.dataset.title, layer)
+              : this.getColorUsers(event.target.classList[1].replace('c', ''), layer);
+            if (users) {
+              tooltip.innerHTML = users;
+              tooltip.style.left = `${event.pageX + 10}px`;
+              tooltip.style.top = `${event.pageY + 10}px`;
+              tooltip.style.display = 'block';
+            }
+          }, 250);
+        }
+      });
+
+      el.addEventListener('mouseout', () => {
+        clearTimeout(this.tooltipTimer);
+        const tooltip = document.getElementById('tooltip');
+        tooltip.style.display = 'none';
+      });
+    });
+  };
+
+  showAnsi = (layer) => {
+    const elem = document.querySelector(`#${layer}-ansicolors`);
+    elem.style.display = elem.style.display === 'flex' ? 'none' : 'flex';
+  };
+
+  generateAnsi256Palette = () => {
+    const rainbow = Array.from({ length: 6 }).map((_, rowIndex) => {
+      return `<div class="ansi-block">${Array.from({ length: 6 })
+        .map((_, colIndex) => {
+          return `<div class="ansi-line">${Array.from({ length: 6 })
+            .map((_, subIndex) => {
+              return `<div class="acbox c${((rowIndex * 6 + colIndex) * 6 + subIndex + 16).toString(16).toUpperCase()}"></div>`;
+            })
+            .join('')}</div>`;
+        })
+        .join('')}</div>`;
+    });
+
+    const grayscale = Array.from({ length: 24 }).map((_, index) => {
+      const grayValue = (index + 232).toString(16).toUpperCase();
+      return `<div class="ansi-line"><div class="acbox c${grayValue}"></div></div>`;
+    });
+
+    document.querySelectorAll('.rainbow').forEach((e) => (e.innerHTML = rainbow.join('')));
+    document.querySelectorAll('.grayscale').forEach((e) => (e.innerHTML = grayscale.join('')));
   };
 
   selectFg = (fg) => {
-    document
-      .querySelector('#fg-colors')
-      .querySelectorAll('.selected')
-      .forEach((el) => el.classList.remove('selected'));
-    document.querySelector('#fg-colors').querySelector(`.c${fg}`).classList.add('selected');
+    document.querySelectorAll('#fg-colors .selected').forEach((el) => el.classList.remove('selected'));
+    document.querySelectorAll('#fg-ansicolors .selected').forEach((el) => el.classList.remove('selected'));
+    document.querySelectorAll('#fg-swatches .selected').forEach((el) => el.classList.remove('selected'));
+
+    if (parseInt(fg, 16) < 16) {
+      document.querySelector(`#fg-colors .c${fg}`).classList.add('selected');
+    } else if (fg.startsWith('#')) {
+      const swatch = document.querySelector(`#fg-swatches .swatch[data-title="${fg}"]`);
+      if (swatch) {
+        swatch.classList.add('selected');
+      } else {
+        console.warn(`Swatch for color ${fg} not found.`);
+      }
+    } else {
+      document.querySelector(`#fg-ansicolors .c${fg}`)?.classList.add('selected');
+    }
   };
 
   selectBg = (bg) => {
-    document
-      .querySelector('#bg-colors')
-      .querySelectorAll('.selected')
-      .forEach((el) => el.classList.remove('selected'));
-    document.querySelector('#bg-colors').querySelector(`.c${bg}`).classList.add('selected');
+    document.querySelectorAll('#bg-colors .selected').forEach((el) => el.classList.remove('selected'));
+    document.querySelectorAll('#bg-ansicolors .selected').forEach((el) => el.classList.remove('selected'));
+    document.querySelectorAll('#bg-swatches .selected').forEach((el) => el.classList.remove('selected'));
+
+    if (parseInt(bg, 16) < 16) {
+      document.querySelector(`#bg-colors .c${bg}`).classList.add('selected');
+    } else if (bg.startsWith('#')) {
+      const swatch = document.querySelector(`#bg-swatches .swatch[data-title="${bg}"]`);
+      if (swatch) {
+        swatch.classList.add('selected');
+      } else {
+        console.warn(`Swatch for color ${bg} not found.`);
+      }
+    } else {
+      document.querySelector(`#bg-ansicolors .c${bg}`)?.classList.add('selected');
+    }
+  };
+
+  updateUsedColors = () => {
+    const usedFgColors = new Set();
+    const usedBgColors = new Set();
+    this.currentTheme.forEach((item) => {
+      if (item.fg) usedFgColors.add(`${item.fg}`);
+      if (item.bg) usedBgColors.add(`${item.bg}`);
+    });
+
+    document.querySelectorAll('.fg .cbox').forEach((el) => {
+      el.classList.remove('used');
+      if (usedFgColors.has(el.classList[1].replace('c', ''))) {
+        el.classList.add('used');
+      }
+    });
+
+    document.querySelectorAll('.bg .cbox').forEach((el) => {
+      el.classList.remove('used');
+      if (usedBgColors.has(el.classList[1].replace('c', ''))) {
+        el.classList.add('used');
+      }
+    });
+
+    document.querySelectorAll('.fg .acbox').forEach((el) => {
+      el.classList.remove('used');
+      if (usedFgColors.has(el.classList[1].replace('c', ''))) {
+        el.classList.add('used');
+      }
+    });
+
+    document.querySelectorAll('.bg .acbox').forEach((el) => {
+      el.classList.remove('used');
+      if (usedBgColors.has(el.classList[1].replace('c', ''))) {
+        el.classList.add('used');
+      }
+    });
+
+    document.querySelectorAll('#fg-swatches .swatch').forEach((el) => {
+      el.classList.remove('used');
+      if (usedFgColors.has(el.dataset.title)) {
+        el.classList.add('used');
+      }
+    });
+
+    document.querySelectorAll('#bg-swatches .swatch').forEach((el) => {
+      el.classList.remove('used');
+      if (usedBgColors.has(el.dataset.title)) {
+        el.classList.add('used');
+      }
+    });
   };
 
   selectColors = (fg, bg) => {
     fg && this.selectBg(bg);
     bg && this.selectFg(fg);
+    this.updateUsedColors();
   };
 
   getHoveredToken = (event) => {
@@ -55,13 +199,18 @@ export default class Configurator {
     });
   };
 
+  selectColorByToken = (tokenName) => {
+    const selected = document.getElementById('selected');
+    selected.textContent = tokenName;
+    const fg = this.currentTheme.find((i) => i.name === selected.textContent)?.fg;
+    const bg = this.currentTheme.find((i) => i.name === selected.textContent)?.bg;
+    this.selectColors(fg, bg);
+  };
+
   bindClick = () => {
     document.querySelector('pre').addEventListener('click', (event) => {
-      const selected = document.getElementById('selected');
-      selected.textContent = this.getHoveredToken(event);
-      const fg = this.currentTheme.find((i) => i.name === selected.textContent)?.fg;
-      const bg = this.currentTheme.find((i) => i.name === selected.textContent)?.bg;
-      this.selectColors(fg, bg);
+      const tokenName = this.getHoveredToken(event);
+      this.selectColorByToken(tokenName);
     });
   };
 
@@ -77,6 +226,32 @@ export default class Configurator {
         }
         this.selectColors(item.fg, item.bg);
         this.applyTheme(this.currentTheme);
+      });
+    });
+
+    document.querySelectorAll('.ansiboxes').forEach((el) => {
+      el.addEventListener('click', (event) => {
+        const color = event.target.classList[1];
+        const layer = event.target.closest('.ansiboxes').id.replace(/-ansicolors$/, '');
+        const selected = document.getElementById('selected');
+        const item = this.currentTheme.find((i) => i.name === selected.textContent);
+        if (selected.textContent !== 'None') {
+          item[layer] = color.replace('c', '');
+        }
+        this.selectColors(item.fg, item.bg);
+        this.applyTheme(this.currentTheme);
+      });
+    });
+
+    document.querySelectorAll('.swatches').forEach((el) => {
+      el.addEventListener('click', (event) => {
+        const layer = event.target.closest('.fg') ? 'fg' : 'bg';
+        const selected = document.getElementById('selected');
+        const color = event.target.dataset.title;
+        if (color.startsWith('#')) {
+          this.currentTheme.find((i) => i.name === selected.textContent)[layer] = color;
+          this.applyTheme(this.currentTheme);
+        }
       });
     });
   };
@@ -240,11 +415,68 @@ export default class Configurator {
       const background = obj.getAttribute('background');
       const foreground = obj.getAttribute('foreground');
       const flags = obj.getAttribute('flags') || '';
+      const bg = this.parseFarColor(background, flags, 'bg');
+      const fg = this.parseFarColor(foreground, flags, 'fg');
 
-      return { name, bg: this.parseFarColor(background, flags, 'bg'), fg: this.parseFarColor(foreground, flags, 'fg') };
+      if (fg.startsWith('#')) {
+        this.swatch.push(fg);
+      }
+
+      if (bg.startsWith('#')) {
+        this.swatch.push(bg);
+      }
+
+      return { name, bg, fg };
     });
 
+    this.swatch = [...new Set(this.swatch)].sort((a, b) => this.getHueValueFromRGB(a) - this.getHueValueFromRGB(b));
+
     return result;
+  };
+
+  getHueValueFromRGB = (hexColor) => {
+    if (!/^#([0-9A-F]{3}|[0-9A-F]{6})$/i.test(hexColor)) {
+      return 0;
+    }
+
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    let hue;
+
+    if (max === min) {
+      hue = 0;
+    } else {
+      const delta = max - min;
+      if (max === r) {
+        hue = ((g - b) / delta + (g < b ? 6 : 0)) * 60;
+      } else if (max === g) {
+        hue = ((b - r) / delta + 2) * 60;
+      } else {
+        hue = ((r - g) / delta + 4) * 60;
+      }
+    }
+
+    return hue;
+  };
+
+  createSwatches = () => {
+    const fgSwatchContainer = document.getElementById('fg-swatches');
+    const bgSwatchContainer = document.getElementById('bg-swatches');
+    fgSwatchContainer.innerHTML = '';
+    bgSwatchContainer.innerHTML = '';
+
+    this.swatch.forEach((color) => {
+      const swatchDiv = document.createElement('div');
+      swatchDiv.className = 'swatch';
+      swatchDiv.style.color = color;
+      swatchDiv.dataset.title = color;
+      fgSwatchContainer.appendChild(swatchDiv.cloneNode(true));
+      bgSwatchContainer.appendChild(swatchDiv);
+    });
   };
 
   applyTheme = (theme) => {
@@ -260,6 +492,15 @@ export default class Configurator {
         root.style.setProperty(`--bg-${name.replace(/\./g, '-')}`, bgValue);
       }
     });
+
+    this.createSwatches();
+
+    const selected = document.getElementById('selected');
+    if (selected.textContent !== 'None') {
+      this.selectColorByToken(selected.textContent);
+    }
+
+    this.updateUsedColors();
   };
 
   browseAndLoadFile = (acceptedExtensions, callback) => {
